85c3fb36ce340436bec8c5a89018c21d15382306
DarumanDev <251592938+DarumanDev@users.noreply.github.com>
2026-07-01 13:42:13 +0200

    Actualització

---
+++ castella/sopa_letras_data.js
@@ -0,0 +1,47 @@
+const WSEARCH_POOL = [
+  // Animales
+  {w:'perro'}, {w:'gato'}, {w:'tigre'}, {w:'lobo'}, {w:'zorro'},
+  {w:'pato'}, {w:'rana'}, {w:'ardilla'}, {w:'conejo'}, {w:'paloma'},
+  {w:'ballena'}, {w:'delfin'}, {w:'caballo'}, {w:'elefante'}, {w:'mariposa'},
+  {w:'tortuga'}, {w:'serpiente'}, {w:'canguro'}, {w:'mosquito'}, {w:'hormiga'},
+  {w:'ciervo'}, {w:'pingüino'}, {w:'cocodrilo'}, {w:'rinoceronte'},
+
+  // Naturaleza y Ciencia
+  {w:'planeta'}, {w:'estrella'}, {w:'montana'}, {w:'volcan'}, {w:'tormenta'},
+  {w:'cometa'}, {w:'pradera'}, {w:'bosque'}, {w:'lluvia'}, {w:'nieve'},
+  {w:'desierto'}, {w:'oceano'}, {w:'terremoto'}, {w:'arcoiris'},
+
+  // Escuela
+  {w:'cuaderno'}, {w:'mochila'}, {w:'maestro'}, {w:'pizarra'}, {w:'ciencia'},
+  {w:'historia'}, {w:'tijeras'}, {w:'lapicero'}, {w:'diccionario'}, {w:'cartulina'},
+
+  // Con H (ortografía)
+  {w:'hueso'}, {w:'hierba'}, {w:'hogar'}, {w:'helado'}, {w:'harina'},
+  {w:'hambre'}, {w:'higiene'}, {w:'hermano'}, {w:'hospital'}, {w:'horario'},
+
+  // Estaciones y tiempo
+  {w:'verano'}, {w:'invierno'}, {w:'primavera'}, {w:'otono'},
+
+  // Familia y personas
+  {w:'familia'}, {w:'abuelo'}, {w:'amigo'}, {w:'vecino'}, {w:'maestro'},
+
+  // Colores y adjetivos
+  {w:'amarillo'}, {w:'naranja'}, {w:'violeta'}, {w:'plateado'},
+
+  // Casa y ciudad
+  {w:'jardin'}, {w:'ciudad'}, {w:'escuela'}, {w:'puerta'}, {w:'ventana'},
+  {w:'cocina'}, {w:'biblioteca'},
+
+  // Animales adicionales
+  {w:'aguila'}, {w:'jirafa'}, {w:'leopardo'}, {w:'camello'}, {w:'flamenco'},
+  {w:'loro'}, {w:'golondrina'}, {w:'nutria'}, {w:'pulpo'}, {w:'coala'},
+
+  // Naturaleza adicional
+  {w:'glaciar'}, {w:'trueno'}, {w:'lago'}, {w:'isla'}, {w:'costa'},
+
+  // Deportes
+  {w:'futbol'}, {w:'tenis'}, {w:'natacion'}, {w:'ciclismo'},
+
+  // Alimentos
+  {w:'manzana'}, {w:'sandia'}, {w:'platano'}, {w:'zanahoria'}, {w:'tomate'},
+];
